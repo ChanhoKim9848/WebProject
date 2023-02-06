23d3d16ecfc2040bcddf1a5f72f3ef8eaedd4b1d
detol3953 <chanhokim9848@gmail.com>
2023-02-06 21:56:00 +0900

    06-02-2023

--- src/main/java/com/detol3953/blog/DummyController.java
+++ src/main/java/com/detol3953/blog/DummyController.java
@@ -1,6 +1,15 @@
 package com.detol3953.blog;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +18,68 @@ import com.detol3953.blog.model.RoleType;
 import com.detol3953.blog.model.User;
 import com.detol3953.blog.repository.UserRepository;
 
+// RestController:  returns not html file but data 
 @RestController
 public class DummyController {
 	
 	@Autowired //의존성 주입(Dependency Insertion)
 	private UserRepository userRepository;
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지당 2건의 데이터를 리턴
+	// one page returns 2 data 
+	
+	// 데이터 2개씩 id에 따라서 최신순으로 정렬
+	// 2 data, sort by id from latest data
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	// {id} 주소로 파라미터를 저장 받을 수 있다 
+	// can get parameters from {id} address
+	// http://localhost:8000/blog/dummy/user/3
+	
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		// user/4을 찾으면 데이터에서 못 찾을 시 user 는 null이 될 것.
+		//  if it cannot find user 4 then user returns null
+		
+		// return null 이 리턴이 되는 문제가 생김.
+		// Optional로  User 객체의 null 여부를 판단해서 return
+		// User Object is optional and check if it is null and return when it is not
+
+		
+		// Lambda equation
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("This user does not exist! id: " +id);
+		});
+		
+//		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+//		@Override
+//		public IllegalArgumentException get() {
+//			// TODO Auto-generated method stub
+//			return new IllegalArgumentException("This user does not exist! id: "+id);
+//		}
+//	});
+		// request: web browser
+		// user object: java object
+		// convert (web browser can understand) to json (Gson Library)
+		// Spring boot = MessageConverter automatically activates when request
+	   // by calling Jackson library which changes user object to json
+		
+		// 스프링부트는 메세지 컨버터가 작동되면서 자바 오브젝트를 json으로 변경해준다
+		return user;
+	}
 	
 	// http://localhost:8000/blog/dummy/join (Request)
 	// http 의 body의 username, password, email 데이터를 가지고 요청하면 
