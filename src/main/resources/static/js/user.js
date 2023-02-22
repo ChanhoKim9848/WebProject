e12749238707944dc0ba72e0b4413c1b818c5e28
detol3953 <chanhokim9848@gmail.com>
2023-02-22 21:23:33 +0900

    22-02-2023

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,23 +1,48 @@
-let index = { 
-	init: function(){
-		$("#btn-save").on("click", ()=>{
+let index = {
+	init: function() {
+		$("#btn-save").on("click", () => { // function(){}, ()=>{} this를 바인딩 하기 위해서 (binding this)
 			this.save();
 		});
 	},
-	
-	save: function(){
+
+	save: function() {
 		// alert("save function called in user")
 		let data = {
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
-		}
-		
-	    // ajax 처리 
-		$.ajax().done().fail()     // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
-		                                          // 3 data username, password and email change to json type with ajax communication
-		                                          //  and request insert
-		                                          // console.log(data) 
+		};
+
+		// ajax 호출시 default가 비동기 호출
+		// calling ajax and its default is asynchronous call
+
+		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		// 3 data username, password and email change to
+		//  json type with ajax communication  and request insert
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http body data
+			
+			contentType: "application/json; charset=utf-8",
+			 // body 데이터가 어떤타입인지  (MIME)
+			//  indicates what type of body data is
+			
+			dataType: "json" // 요청을  서버로해서 응답이 왔을때 데이터 문자열이 json 타입이라면 JavaScript 오브젝트로 변경.
+			// if the data request from the server is json type then changes to JavaScript object.
+
+			// 회원가입 수행 요청 - Registeration Request
+		}).done(function(resp) {   // done
+			alert("Registered Successfully!");
+			console.log(resp);
+
+			// 회원가입 후 메인화면 리턴
+			// return m ain page after the register
+			//location.href = "/blog";
+
+		}).fail(function(error) {    // fail
+			alert(JSON.stringify(error));
+		});
 	}
 }
 
